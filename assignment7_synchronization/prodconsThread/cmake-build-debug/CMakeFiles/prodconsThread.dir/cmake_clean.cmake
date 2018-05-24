2a10aad663e1f7927a0ab2ce9e27a2d262fec3bb
Kim Minho <minmin0416@naver.com>
2018-05-24 19:56:03 +0900

    Add prodconsThread

---
+++ assignment7_synchronization/prodconsThread/cmake-build-debug/CMakeFiles/prodconsThread.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prodconsThread.dir/main.cpp.o"
+  "prodconsThread.pdb"
+  "prodconsThread"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prodconsThread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
